4e72865c53621f6b49fbf757b5db45a41ff1f1ab
Heather <heather.craddock@hotmail.co.uk>
2020-09-30 11:57:01 -0400

    solution

--- src/main/java/cd/get/ready/algorithms/EuclidsAlgorithm.java
+++ src/main/java/cd/get/ready/algorithms/EuclidsAlgorithm.java
@@ -3,7 +3,13 @@ package cd.get.ready.algorithms;
 public class EuclidsAlgorithm {
 
     public int gcd(int a, int b){
-        return -1;
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a%b);
+        }
     }
 
 }
